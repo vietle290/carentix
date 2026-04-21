07fe3ef2ba1e9b34b6d8d8a9f74095c2e93aa155
Vietle290 <lehoangviet2970@gmail.com>
2026-04-21 08:21:51 +0700

    enhancing Navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,28 @@
 "use client";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import AuthModal from "./AuthModal";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { Bike, Car, ChevronRight, LogOut, Menu, Truck, X } from "lucide-react";
+import { signOut } from "next-auth/react";
 const Nav_Items = ["Home", "Bookings", "About Us", "Contact"];
 function Nav() {
   const pathName = usePathname();
   const [authOpen, setAuthOpen] = useState(false);
+  const { userData } = useSelector((state: RootState) => state.user);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogOut = async () => {
+    await signOut({ redirect: false });
+    setProfileOpen(false);
+    dispatch({ type: "user/setUserData", payload: null });
+  };
   return (
     <>
       <motion.div
@@ -46,14 +60,197 @@ function Nav() {
               );
             })}
           </div>
-          <button
-            className="px-4 py-1.5 rounded-full bg-white text-black text-sm font-semibold hover:bg-gray-200 transition cursor-pointer"
-            onClick={() => setAuthOpen(true)}
-          >
-            Login
-          </button>
+          <div className="flex items-center gap-3 relative">
+            <div className="hidden md:block relative">
+              {!userData ? (
+                <button
+                  className="px-4 py-1.5 rounded-full bg-white text-black text-sm font-semibold hover:bg-gray-200 transition cursor-pointer"
+                  onClick={() => setAuthOpen(true)}
+                >
+                  Login
+                </button>
+              ) : (
+                <>
+                  <button
+                    type="button"
+                    className="w-11 h-11 rounded-full bg-white text-black font-bold"
+                    onClick={() => setProfileOpen((prev) => !prev)}
+                  >
+                    {userData.name.charAt(0).toUpperCase()}
+                  </button>
+                  <AnimatePresence>
+                    {profileOpen && (
+                      <motion.div
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        className="absolute top-14 right-0 w-75 bg-white text-black rounded-2xl shadow-xl border"
+                      >
+                        <div className="p-5 ">
+                          <p className="font-semibold text-lg">
+                            {userData.name}
+                          </p>
+                          <p className="text-xs uppercase text-gray-500 mb-4">
+                            {userData.role}
+                          </p>
+                          {userData.role !== "partner" && (
+                            <div className="w-full flex items-center gap-3 py-3 hover:bg-gray-100 rounded-xl">
+                              <div className="flex -space-x-2 ml-1">
+                                <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center">
+                                  <Bike size={14} className="text-gray-500" />
+                                </div>
+                                <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center">
+                                  <Car size={14} className="text-gray-500" />
+                                </div>
+                                <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center">
+                                  <Truck size={14} className="text-gray-500" />
+                                </div>
+                              </div>
+                              Become a Partner
+                              <ChevronRight size={16} className="ml-auto" />
+                            </div>
+                          )}
+                          <button
+                            type="button"
+                            className="w-full flex items-center gap-3 py-3 hover:bg-gray-100 rounded-xl mt-2"
+                            onClick={handleLogOut}
+                          >
+                            <LogOut size={16} className="ml-1" />
+                            Logout
+                          </button>
+                        </div>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </>
+              )}
+            </div>
+
+            <div className="md:hidden">
+              {!userData ? (
+                <button
+                  className="px-4 py-1.5 rounded-full bg-white text-black text-sm font-semibold hover:bg-gray-200 transition cursor-pointer"
+                  onClick={() => setAuthOpen(true)}
+                >
+                  Login
+                </button>
+              ) : (
+                <>
+                  <button
+                    type="button"
+                    className="w-11 h-11 rounded-full bg-white text-black font-bold"
+                    onClick={() => setProfileOpen((prev) => !prev)}
+                  >
+                    {userData.name.charAt(0).toUpperCase()}
+                  </button>
+                </>
+              )}
+            </div>
+
+            <button
+              type="button"
+              className="md:hidden text-white"
+              onClick={() => setMenuOpen((prev) => !prev)}
+            >
+              {menuOpen ? <X size={26} /> : <Menu />}
+            </button>
+          </div>
         </div>
       </motion.div>
+      <AnimatePresence>
+        {menuOpen && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.4 }}
+              exit={{ opacity: 0 }}
+              onClick={() => setMenuOpen(false)}
+              className="fixed inset-0 bg-black z-30 md:hidden"
+            />
+
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.2 }}
+              className="fixed top-21.25 left-1/2 -translate-x-1/2 w-[92%] bg-[#0B0B0B] rounded-2xl shadow-2xl z-40 md:hidden overflow-hidden"
+            >
+              <div className="flex flex-col divide-y divide-white/10">
+                {Nav_Items.map((item, index) => {
+                  let href;
+                  if (item === "Home") {
+                    href = "/";
+                  } else {
+                    href = `/${item.toLowerCase()}`;
+                  }
+                  const active = pathName === href;
+                  return (
+                    <Link
+                      key={index}
+                      href={href}
+                      className="px-6 py-4 text-gray-300 hover:bg-white/5"
+                    >
+                      {item}
+                    </Link>
+                  );
+                })}
+              </div>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {profileOpen && userData && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.4 }}
+              exit={{ opacity: 0 }}
+              onClick={() => setProfileOpen(false)}
+              className="fixed inset-0 bg-black z-30 md:hidden"
+            />
+            <motion.div
+              initial={{ y: 400 }}
+              animate={{ y: 0 }}
+              exit={{ y: 400 }}
+              transition={{ type: "spring", damping: 25, stiffness: 500 }}
+              className="fixed inset-x-0 bottom-0 bg-white rounded-t-3xl shadow-2xl z-50 md:hidden"
+            >
+              <div className="p-5 ">
+                <p className="font-semibold text-lg">{userData.name}</p>
+                <p className="text-xs uppercase text-gray-500 mb-4">
+                  {userData.role}
+                </p>
+                {userData.role !== "partner" && (
+                  <div className="w-full flex items-center gap-3 py-3 hover:bg-gray-100 rounded-xl">
+                    <div className="flex -space-x-2 ml-1">
+                      <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center">
+                        <Bike size={14} className="text-gray-500" />
+                      </div>
+                      <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center">
+                        <Car size={14} className="text-gray-500" />
+                      </div>
+                      <div className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center">
+                        <Truck size={14} className="text-gray-500" />
+                      </div>
+                    </div>
+                    Become a Partner
+                    <ChevronRight size={16} className="ml-auto" />
+                  </div>
+                )}
+                <button
+                  type="button"
+                  className="w-full flex items-center gap-3 py-3 hover:bg-gray-100 rounded-xl mt-2"
+                  onClick={handleLogOut}
+                >
+                  <LogOut size={16} className="ml-1" />
+                  Logout
+                </button>
+              </div>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
       <AuthModal open={authOpen} onClose={() => setAuthOpen(false)} />
     </>
   );
