9151feb562f97fa7bb0b9a89676fdad53b58520f
Vietle290 <lehoangviet2970@gmail.com>
2026-04-22 10:26:26 +0700

    add vehicle model

---
+++ src/models/vehicle.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+type vehicleType = "bike" | "car" | "loading" | "truck" | "auto";
+
+interface IVehicle {
+    owner: mongoose.Types.ObjectId;
+    type: vehicleType;
+    vehicleModel: string;
+    number: string;
+    imageUrl?: string;
+    baseFare?: number;
+    pricePerKM?: number;
+    waitingCharge?: number;
+    status: "approved" | "pending" | "rejected";
+    rejectionReason?: string;
+    isActive: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const vehicleSchema = new mongoose.Schema<IVehicle>({
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: { type: String, enum: ["bike", "car", "loading", "truck", "auto"], required: true },
+    vehicleModel: { type: String, required: true },
+    number: { type: String, required: true, unique: true },
+    imageUrl: { type: String },
+    baseFare: { type: Number },
+    pricePerKM: { type: Number },
+    waitingCharge: { type: Number },
+    status: { type: String, enum: ["approved", "pending", "rejected"], default: "pending" },
+    rejectionReason: { type: String },
+    isActive: { type: Boolean, default: true },
+},{timestamps: true});
+
+const Vehicle = mongoose.models.Vehicle || mongoose.model("Vehicle", vehicleSchema);
+export default Vehicle;
